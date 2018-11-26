f9c8710b426e3963025fa0383638751c85a6a779
Battlehub0x <Vadim.Andriyanov@outlook.com>
2018-11-26 18:56:36 +0200

    Dock panels implementation

---
+++ Assets/Battlehub/UIControls/DockPanels/Scripts/Region.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Battlehub.UIControls.DockPanels
+{
+    public enum RegionSplitType
+    {
+        Left,
+        Top,
+        Right,
+        Bottom
+    }
+
+    public class Region : Selectable
+    {
+        [SerializeField]
+        private ToggleGroup m_headerPanel;
+
+        [SerializeField]
+        private RectTransform m_contentPanel;
+
+        [SerializeField]
+        private Tab m_tabPrefab;
+
+        [SerializeField]
+        private Region m_regionPrefab;
+
+        public void Add(Sprite icon, string header, RectTransform content)
+        {
+            Tab tab = Instantiate(m_tabPrefab, m_headerPanel.transform);
+            tab.Icon = icon;
+            tab.Text = header;
+            tab.ToggleGroup = m_headerPanel;
+            content.SetParent(m_contentPanel, false);
+
+            tab.Toggle += OnTabToggle;
+            tab.IsOn = true;
+        }
+
+        
+
+        public void Split(RegionSplitType splitType)
+        {
+            switch (splitType)
+            {
+                case RegionSplitType.Left:
+                    break;
+                case RegionSplitType.Top:
+                    break;
+                case RegionSplitType.Right:
+                    break;
+                case RegionSplitType.Bottom:
+                    break;
+            }
+        }
+
+        private void OnTabToggle(Tab sender, bool args)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
---
+++ Assets/Battlehub/UIControls/DockPanels/Scripts/Tab.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Battlehub.UIControls.DockPanels
+{
+    public class PointerEventArgs
+    {
+        public PointerEventData EventData
+        {
+            get;
+            private set;
+        }
+
+        public PointerEventArgs(PointerEventData eventData)
+        {
+            EventData = eventData;
+        }
+    }
+
+    public delegate void TabEventArgs<T>(Tab sender, T args);
+
+    public class Tab : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        public event TabEventArgs<bool> Toggle;
+        public event TabEventArgs<PointerEventData> BeginDrag;
+        public event TabEventArgs<PointerEventData> Drag;
+        public event TabEventArgs<PointerEventData> EndDrag;
+
+        [SerializeField]
+        private Image m_img;
+
+        [SerializeField]
+        private Text m_text;
+
+        [SerializeField]
+        private Toggle m_toggle;
+
+        public Sprite Icon
+        {
+            get { return m_img.sprite; }
+            set { m_img.sprite = value; }
+        }
+
+        public string Text
+        {
+            get { return m_text.text; }
+            set { m_text.text = value; }
+        }
+
+        public ToggleGroup ToggleGroup
+        {
+            get { return m_toggle.group; }
+            set { m_toggle.group = value; }
+        }
+
+        public bool IsOn
+        {
+            get { return m_toggle.isOn; }
+            set { m_toggle.isOn = value; }
+        }
+
+        private void Awake()
+        {
+            m_toggle.onValueChanged.AddListener(OnToggleValueChanged);
+        }
+
+        private void OnDestroy()
+        {
+            if(m_toggle != null)
+            {
+                m_toggle.onValueChanged.RemoveListener(OnToggleValueChanged);
+            }
+        }
+
+        private void OnToggleValueChanged(bool value)
+        {
+            if(Toggle != null)
+            {
+                Toggle(this, value);
+            }
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            Debug.Log("BeginDrag");
+            if(BeginDrag != null)
+            {
+                BeginDrag(this, eventData);
+            }
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            Debug.Log("Drag");
+            if(Drag != null)
+            {
+                Drag(this, eventData);
+            }
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            Debug.Log("EndDrag");
+            if(EndDrag != null)
+            {
+                EndDrag(this, eventData);
+            }
+        }
+    }
+
+}
